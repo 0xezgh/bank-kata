4d15ded08e2cd952704eb4d5d84902e4d7e10fe5
Administrator <administrator@MacBook-Air-de-Administrator-4.local>
2018-12-03 16:14:27 +0100

    added operationRepository specs

---
+++ tests/operationRepository.spec.js
@@ -0,0 +1,18 @@
+import OperationRepository from '../src/operationRepository';
+
+const chai = require('chai');
+// eslint-disable-next-line prefer-destructuring
+const assert = chai.assert;
+
+const OPERATIONS = [{ amount: -100, date: new Date() }, { amount: 200, date: new Date() }];
+
+const operationRepository = new OperationRepository();
+
+describe('OperationRepository', () => {
+  it('stores operations', () => {
+    OPERATIONS.forEach(
+      operation => operationRepository.saveOperation(operation.amount, operation.date),
+    );
+    assert.sameDeepMembers(OPERATIONS, operationRepository.allOperations());
+  });
+});
