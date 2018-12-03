27a249e425f87dffd7360aeb1e1a3b063617a983
Administrator <administrator@MacBook-Air-de-Administrator-4.local>
2018-12-03 16:47:27 +0100

    added tests for operationPrinter

---
+++ tests/operationPrinter.spec.js
@@ -0,0 +1,35 @@
+import OperationPrinter from '../src/operationPrinter';
+
+const sinon = require('sinon');
+const chai = require('chai');
+
+// eslint-disable-next-line prefer-destructuring
+const expect = chai.expect;
+chai.use(require('sinon-chai'));
+
+const OPERATIONS = [{ amount: 200, date: Date.parse('2018-12-02') },
+  { amount: -26, date: Date.parse('2018-12-22') }];
+
+let operationPrinter;
+
+describe('OperationPrinter', () => {
+  beforeEach(
+    () => {
+      operationPrinter = new OperationPrinter();
+      sinon.spy(console, 'log');
+    },
+  );
+  afterEach(() => {
+    console.log.restore();
+  });
+  it('prints the header', () => {
+    operationPrinter.print([]);
+    expect(console.log).to.have.been.calledWith('DATE | AMOUNT | BALANCE');
+  });
+
+  it('prints one transaction per line', () => {
+    operationPrinter.print(OPERATIONS);
+    expect(console.log).to.have.been.calledWith('08/07/2005 | 200 | 200');
+    expect(console.log).to.have.been.calledWith('21/07/2015 | -26 | 174');
+  });
+});
