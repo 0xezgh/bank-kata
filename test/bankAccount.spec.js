170a9d69da1593310513aa325960e01aec4e623d
ezdin <ezdine.gharbi@gmail.com>
2018-12-15 23:11:34 +0100

    refacotr: using one account instantiatedfollowing need with stubbed repo or normal repo

--- test/bankAccount.spec.js
+++ test/bankAccount.spec.js
@@ -16,8 +16,7 @@ const OPERATIONS = [{ amount: 50 }, { amount: 100 }];
 const operationRepository = new OperationRepository();
 const operationRepositoryStub = sinon.createStubInstance(OperationRepository);
 const operationPrinter = sinon.createStubInstance(OperationPrinter);
-const bankAccountWithStubbedRepo = new BankAccount(operationRepositoryStub, operationPrinter);
-const bankAccountWitRepo = new BankAccount(operationRepository, operationPrinter);
+
 
 describe('BankAccount', () => {
   beforeEach(
@@ -32,8 +31,9 @@ describe('BankAccount', () => {
 
   it('should accept a deposit when the amount is positive and superior or equal to the account balance', () => {
     operationRepositoryStub.allOperations.onFirstCall().returns(OPERATIONS);
+    const bankAccount = new BankAccount(operationRepositoryStub, operationPrinter);
 
-    bankAccountWithStubbedRepo.deposit(1000);
+    bankAccount.deposit(1000);
 
     expect(operationRepositoryStub.saveOperation).to.have.been.calledWith(1000, new Date());
   });
@@ -41,9 +41,10 @@ describe('BankAccount', () => {
   it('should properly set the new account balance when deposit amount is valid', () => {
     const initOperationsForDepositTest = [{ amount: 50 }, { amount: 100 }];
     operationRepository.setOperations(initOperationsForDepositTest);
+    const bankAccount = new BankAccount(operationRepository, operationPrinter);
 
-    bankAccountWitRepo.deposit(1000);
-    const operationsToCheck = bankAccountWitRepo.getBalanceHistory();
+    bankAccount.deposit(1000);
+    const operationsToCheck = bankAccount.getBalanceHistory();
 
     expect(operationsToCheck[2].balance).to.equal(1150);
   });
@@ -51,9 +52,10 @@ describe('BankAccount', () => {
   it('should properly set the new account balance when withdrawal amount is valid', () => {
     const initOperationsForWithdrawtTest = [{ amount: 50 }, { amount: 100 }];
     operationRepository.setOperations(initOperationsForWithdrawtTest);
+    const bankAccount = new BankAccount(operationRepository, operationPrinter);
 
-    bankAccountWitRepo.withdraw(150);
-    const operationsToCheck = bankAccountWitRepo.getBalanceHistory();
+    bankAccount.withdraw(150);
+    const operationsToCheck = bankAccount.getBalanceHistory();
 
     expect(operationsToCheck[2].balance).to.equal(0);
   });
@@ -61,8 +63,9 @@ describe('BankAccount', () => {
   it('should not accept a deposit if the amount is not positive', () => {
     const saveOperationSpy = sinon.spy(operationRepositoryStub.saveOperation);
     operationRepositoryStub.allOperations.onFirstCall().returns(OPERATIONS);
+    const bankAccount = new BankAccount(operationRepositoryStub, operationPrinter);
 
-    bankAccountWithStubbedRepo.deposit(-1000);
+    bankAccount.deposit(-1000);
 
     expect(console.log).to.have.been.calledWith(-1000, ' :DEPOSIT AMOUNT MUST BE STRICTLY POSITIVE');
     expect(saveOperationSpy.callCount).to.equal(0);
@@ -70,8 +73,9 @@ describe('BankAccount', () => {
 
   it('should accept a withdrawal when the amount is positive and inferior or equal to the account balance', () => {
     operationRepositoryStub.allOperations.onFirstCall().returns(OPERATIONS);
+    const bankAccount = new BankAccount(operationRepositoryStub, operationPrinter);
 
-    bankAccountWithStubbedRepo.withdraw(1000);
+    bankAccount.withdraw(1000);
 
     expect(operationRepositoryStub.saveOperation).to.have.been.calledWith(-1000, new Date());
   });
@@ -79,8 +83,9 @@ describe('BankAccount', () => {
   it('should not accept a withdrawal if the amount is not positive', () => {
     operationRepositoryStub.allOperations.onFirstCall().returns(OPERATIONS);
     const saveOperationSpy = sinon.spy(operationRepositoryStub.saveOperation);
+    const bankAccount = new BankAccount(operationRepositoryStub, operationPrinter);
 
-    bankAccountWithStubbedRepo.withdraw(-1000);
+    bankAccount.withdraw(-1000);
 
     expect(console.log).to.have.been.calledWith(-1000, ' :WITHDRAWAL AMOUNT MUST BE STRICTLY POSITIVE');
     expect(saveOperationSpy.callCount).to.equal(0);
@@ -88,8 +93,9 @@ describe('BankAccount', () => {
 
   it('should print a statement', () => {
     operationRepositoryStub.allOperations.onFirstCall().returns(OPERATIONS);
+    const bankAccount = new BankAccount(operationRepositoryStub, operationPrinter);
 
-    bankAccountWithStubbedRepo.printOperations();
+    bankAccount.printOperations();
 
     expect(operationPrinter.print).to.have.been.calledWith(OPERATIONS);
   });
